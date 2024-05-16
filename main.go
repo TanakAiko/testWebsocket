80e0b89224ddd30102d5710729248eae2f4a673f
TanakAiko <cherifmbaye02@gmail.com>
2024-05-16 13:05:27 +0000

    it works with the example

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -28,14 +25,12 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	// Mise à niveau de la connexion HTTP en WebSocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		//log.Fatalf("Failed to set up WebSocket upgrade: %v", err)
 		fmt.Println(err)
 		return
 	}
 	defer ws.Close()
 
 	for {
-
 		messageType, data, err := ws.ReadMessage()
 		if err != nil {
 			fmt.Printf("Error reading msg: %v", err)
@@ -43,33 +38,16 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		if ws.WriteMessage(messageType, data); err != nil {
+		fmt.Printf("Received message: %s\n", data)
+
+		if err := ws.WriteMessage(messageType, data); err != nil {
 			fmt.Println(err)
-			return
-		}
-
-		var msg ScoreData
-
-		err = json.Unmarshal(data, &msg)
-		if err != nil {
-			fmt.Printf("Error when Unmarshal data: %v", err)
-			return
-		}
-
-		// Validation des données reçues
-		if msg.Name == "" || msg.Score < 0 || msg.Time < 0 {
-			fmt.Println("Invalid data received !")
-			return
-		}
-
-		if err := writeJSONToFile(msg); err != nil {
-			fmt.Printf("Error writing JSON to file: %v", err)
 			return
 		}
 	}
 }
 
-func writeJSONToFile(data ScoreData) error {
+/* func writeJSONToFile(data ScoreData) error {
 	var scores []ScoreData
 
 	// Lecture du fichier existant
@@ -99,19 +77,19 @@ func writeJSONToFile(data ScoreData) error {
 
 	// Ecriture des données mises à jour dans le fichier
 	if err := os.WriteFile("score.json", content, 0644); err != nil {
-		fmt.Printf("Error writing JSON to file: %v", err)
+		log.Printf("Error writing JSON to file: %v", err)
 		return err
 	}
 
-	fmt.Println("Data successfully written to score.json")
+	log.Println("Data successfully written to score.json")
 	return nil
-}
+} */
 
 func main() {
 	http.HandleFunc("/ws", handleConnections)
 	/* http.HandleFunc("/", home) */
 	fmt.Println("WebSocket server starting on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		fmt.Println(err)
 	}
 }
