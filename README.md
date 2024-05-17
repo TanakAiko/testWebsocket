798edfcca19e8e4d60dce49550abb767e29e237a
TanakAiko <cherifmbaye02@gmail.com>
2024-05-17 11:27:01 +0000

    Done with this repo, for now

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # testWebsocket
-Learn about websocket
+Learn about websocket.
+
+For testing just run `go run main.go` !!!
--- main.go
+++ main.go
@@ -26,7 +26,6 @@ var upgrader = websocket.Upgrader{
 }
 
 func handleWebsocket(w http.ResponseWriter, r *http.Request) {
-	// Mise à niveau de la connexion HTTP en WebSocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -42,8 +41,6 @@ func handleWebsocket(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		fmt.Printf("Received message: %s\n", data)
-
 		if err := ws.WriteMessage(messageType, data); err != nil {
 			fmt.Println(err)
 			return
@@ -57,9 +54,6 @@ func handleWebsocket(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		fmt.Println(msg, "HERREEE msg")
-
-		// Validation des données reçues
 		if msg.Name == "" || msg.Score < 0 || msg.Time < 0 {
 			fmt.Println("Invalid data received !")
 			return
@@ -69,15 +63,14 @@ func handleWebsocket(w http.ResponseWriter, r *http.Request) {
 			fmt.Printf("Error writing JSON to file: %v", err)
 			return
 		}
-
 	}
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	/* if r.URL.Path != "/" {
+	if r.URL.Path != "/" {
 		http.Error(w, "Page not found", http.StatusNotFound)
 		return
-	} */
+	}
 
 	if r.Method != http.MethodGet {
 		http.Error(w, "Not allowed", http.StatusMethodNotAllowed)
@@ -100,13 +93,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func writeJSONToFile(data ScoreData) error {
-	// Lecture du fichier existant
 	content, err := os.ReadFile("score.json")
 	if err != nil && !os.IsNotExist(err) {
 		return err
 	}
 
-	// Désérialisation des données existantes si le fichier n'est pas vide
 	var scores []ScoreData
 	if len(content) > 0 {
 		if err := json.Unmarshal(content, &scores); err != nil {
@@ -114,16 +105,13 @@ func writeJSONToFile(data ScoreData) error {
 		}
 	}
 
-	// Ajout des nouvelles données
 	scores = append(scores, data)
 
-	// Sérialisation des données mises à jour
 	content, err = json.MarshalIndent(scores, "", " ")
 	if err != nil {
 		return err
 	}
 
-	// Ecriture des données mises à jour dans le fichier
 	if err := os.WriteFile("score.json", content, 0644); err != nil {
 		return err
 	}
@@ -135,10 +123,6 @@ func writeJSONToFile(data ScoreData) error {
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/ws", handleWebsocket)
-
-	// Gestion des fichiers statiques
-	fs := http.FileServer(http.Dir("."))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	fmt.Println("WebSocket server starting on port http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
